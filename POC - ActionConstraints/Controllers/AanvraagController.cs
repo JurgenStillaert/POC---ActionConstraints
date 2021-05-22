c9bd9eb70ccd8e6966e812ec2f678805c10833a5
Jurgen Stillaert <jurgen@stillaert.be>
2021-05-22 10:13:00 +0200

    Add project files.

---
+++ POC - ActionConstraints/Controllers/AanvraagController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POC___ActionConstraints.Infrastructure;
+
+namespace POC___ActionConstraints.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class AanvraagController : Controller
+	{
+		[HttpPatch, HttpHeader("LV-Command", "Indienen")]
+		public IActionResult Indienen()
+		{
+			return Ok();
+		}
+
+		[HttpPatch, HttpHeader("LV-Command", "InBehandelingZetten")]
+		public IActionResult InBehandelingZetten()
+		{
+			return Ok();
+		}
+	}
+}
---
+++ POC - ActionConstraints/Infrastructure/HttpHeaderAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using System;
+
+namespace POC___ActionConstraints.Infrastructure
+{
+	//https://stackoverflow.com/questions/39302121/header-based-routing-in-asp-net-core
+	public sealed class HttpHeaderAttribute : Attribute, IActionConstraint
+	{
+		public string Header { get; set; }
+		public string Value { get; set; }
+
+		public HttpHeaderAttribute(string header, string value)
+		{
+			Header = header;
+			Value = value;
+		}
+
+		public bool Accept(ActionConstraintContext context)
+		{
+			if (context.RouteContext.HttpContext.Request.Headers.TryGetValue(Header, out var value))
+			{
+				return value[0] == Value;
+			}
+
+			return false;
+		}
+
+		public int Order => 0;
+	}
+}
